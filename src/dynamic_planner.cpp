728c6d02f219f6563548cd54f57929273772cc22
ssteveminq <steveminq@gmail.com>
2018-02-12 23:43:30 -0600

    first updated

---
+++ src/dynamic_planner.cpp
@@ -0,0 +1,98 @@
+#include "ros/ros.h"
+#include "Dynamic_Manager.h"
+#include "std_msgs/String.h"
+#include "std_msgs/Int8.h"
+#include "geometry_msgs/Pose2D.h"
+#include "geometry_msgs/PointStamped.h"
+#include "geometry_msgs/PoseStamped.h"
+#include "sensor_msgs/JointState.h"
+#include <nav_msgs/OccupancyGrid.h>
+#include <nav_msgs/Odometry.h>
+#include <visualization_msgs/Marker.h>
+#include <visualization_msgs/MarkerArray.h>
+#include <eigen3/Eigen/Dense>
+// #include <Eigen/Dense>
+#include <sstream>
+#include <boost/thread/thread.hpp>
+#include "srBSpline.h"
+
+using namespace Eigen;
+
+bool boolSolve=false;
+MapParam   dynamicmapParam(10,10,0.75);
+static int  Receive_count=0;
+
+void CmdIntCallback(const std_msgs::Int8::ConstPtr& msg)
+{
+
+
+}
+
+int main(int argc, char **argv)
+{
+  ros::init(argc, argv, "dynamic_planner");
+  
+  Dynamic_Manager dynamicManager; 
+  //problemmanager.setRosObj(&nodeObj);
+  dynamicManager.setPMapParam(&dynamicmapParam);
+
+  // ros::Rate r(5);
+  ros::Subscriber Point_sub;          //subscriber clicked point
+  ros::Subscriber SplinePath_sub;
+  ros::Subscriber dynamicmap_sub;
+  ros::Subscriber global_pos_sub;
+  ros::Subscriber jointstates_sub;
+  ros::Subscriber  human_yolo_sub;
+  //ros::Subscriber edge_leg_sub;
+  ros::Subscriber filtered_result_sub;
+  //ros::Subscriber keyboard_sub;
+  ros::NodeHandle n;
+  
+  //Point_sub        = n.subscribe<geometry_msgs::PointStamped>("/clicked_point", 10, &Dynamic_Manager::ClikedpointCallback,&dynamicManager);
+  //human_cmd_sub    = n.subscribe<std_msgs::Int8>("/Int_cmd_trackhuman", 50, &Dynamic_Manager::Human_target_cmdCallback,&dynamicManager);
+  //human_marker_sub = n.subscribe<visualization_msgs::Marker>("/human_target", 50, &Dynamic_Manager::Human_MarkerCallback,&dynamicManager);
+  //human_yolo_sub   = n.subscribe<visualization_msgs::MarkerArray>("/human_boxes", 10, &Dynamic_Manager::Human_Yolo_Callback,&dynamicManager);
+  //human_leg_sub    = n.subscribe<geometry_msgs::PoseArray>("/edge_leg_detector", 20, &Dynamic_Manager::human_leg_callback,&dynamicManager);
+  
+  dynamicmap_sub   = n.subscribe<nav_msgs::OccupancyGrid>("/dynamic_obstacle_map_ref", 30, &Dynamic_Manager::dynamic_mapCallback,&dynamicManager); 
+  jointstates_sub  = n.subscribe<sensor_msgs::JointState>("/hsrb/joint_states", 10, &Dynamic_Manager::joint_states_callback,&dynamicManager);
+  global_pos_sub   = n.subscribe<geometry_msgs::PoseStamped>("/global_pose", 10, &Dynamic_Manager::global_pose_callback,&dynamicManager);
+  SplinePath_sub=  n.subscribe<nav_msgs::Path>("/mdp_path", 10, &Dynamic_Manager::mdppath_callback,&dynamicManager);
+  human_yolo_sub   = n.subscribe<visualization_msgs::MarkerArray>("/human_boxes", 10, &Dynamic_Manager::Human_MarkerArrayCallback,&dynamicManager);
+
+  // filter_result_sub=nh_.subscribe<people_msgs::PositionMeasurement>("people_tracker_filter", 10,&Edgeleg_manager::filter_result_callback,&dynamicManager);
+
+  ros::Rate loop_rate(30);
+  while (ros::ok())
+  {
+      //problemmanager.MDPsolPublish();
+       // if(dynamicManager.m_boolSolve)
+       // {
+       //    ROS_INFO("Start to dynamic mdp");
+       //    dynamicManager.MDPsolve(); 
+       //    dynamicManager.generate_dynamicPath();
+       //    dynamicManager.m_boolSolve=false;
+       //    // dynamicManager.booltrackHuman=false;
+       //    dynamicManager.dyn_path_num++;
+       // }
+       // else
+       // {
+       //   if(dynamicManager.dyn_path_num>0)
+       //     dynamicManager.publishpaths();
+       // }
+
+     //dynamicManager.set_dynamicPath();     
+     dynamicManager.publish_cameraregion();
+   
+  	 ros::spinOnce();
+     loop_rate.sleep();  
+  }
+
+  ros::spin();
+
+  return 0;
+}
+
+
+
+
